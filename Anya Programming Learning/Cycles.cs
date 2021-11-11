b75c9f198c41c2d5272455ebba1b2dff7004fb70
annkostet <annakostetska30@gmail.com>
2021-11-11 21:34:31 +0200

    HomeWork Loops

---
+++ Anya Programming Learning/Cycles.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Anya_Programming_Learning
+{
+	public class Cycles
+	{
+		public static double AtopowerB(double A, double B)
+		{
+			double count = 1;
+				for (int i = 1; i < B; i++)
+			{
+				count *= A; 
+			}
+			return count;
+		}
+	}
+}
