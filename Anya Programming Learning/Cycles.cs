afa1ac31d9cf1643f1f16184cf71cf9ecdecaa24
annkostet <annakostetska30@gmail.com>
2021-11-12 23:11:47 +0200

    homework 5

--- Anya Programming Learning/Cycles.cs
+++ Anya Programming Learning/Cycles.cs
@@ -2,16 +2,67 @@
 
 namespace Anya_Programming_Learning
 {
-	public class Cycles
-	{
-		public static double AtopowerB(double A, double B)
-		{
-			double count = 1;
-				for (int i = 1; i < B; i++)
-			{
-				count *= A; 
-			}
-			return count;
-		}
-	}
+    public class Cycles
+    {
+        public static double GetAToPowerB(double A, double B)
+        {
+            double count = 1;
+            for (double i = 1; i <= B; i++)
+        {
+            count *= A; 
+        }
+
+            return count;
+        }
+
+        public static double GetADevidedNumbers (double A)
+        {
+            double count = 1;
+            for (double i = 1; i <= 1000; i++)
+            {
+            if( i % A == 0)
+            { 
+            count++;
+            }
+        }
+
+            return count;
+        }
+        public static int GetPositiveNumbers(int A)
+        {
+            int count = 1;
+            for (int i = 1; i > 0; i++)
+        {
+            if (i*i < A)
+            {
+            count++;
+            }
+        }
+
+            return count;
+        //}
+        //public static int GetGreatestDivisorOfA(int A) // TO BE DONE
+        //{ }
+        //public static int GetSumDevidedonSeven(int A, int B) // TO BE DONE
+        //{
+        //	int count = 1;
+        //              for (int i = 0; i < length; i++)
+        //          {
+
+        //          }
+    }
+    public static GetFibonachiLine(int N)
+    {
+        int A = 0;
+        int B = 1;
+        for (int i = 1; i < N; i++)
+        {
+            int sum = A + B;
+            A = B;
+            B = sum;
+        }
+
+        return B;
+    }
+}
 }
--- Anya Programming Learning/Program.cs
+++ Anya Programming Learning/Program.cs
@@ -4,33 +4,38 @@ namespace Anya_Programming_Learning
 {
     public class Program
     {
+        static void Main ()
+        {
+            Console.WriteLine(Cycles.GetAToPowerB(5,5));
+        }
+
         public static double Task1(int A, int B)
         {
             return ((5 * A) + (B * B)) / (B - A);
         }
 
-        public static void Task2(ref string A, ref string B)
+            public static void Task2(ref string A, ref string B)
         {
             string temp = A;
             A = B;
             B = temp;
         }
 
-        public static(int division, int reminder) Task3(int A, int B)
+            public static (int division, int reminder) Task3(int A, int B)
         {
             return (A / B, A % B);
         }
-        public static double Task4(double A, double B, double C)
+
+            public static double Task4(double A, double B, double C)
         {
             return (C - B) / A;
         }
-        public static (double A, double B) Task5(double X1, double Y1, double X2, double Y2)
+
+            public static (double A, double B) Task5(double X1, double Y1, double X2, double Y2)
         {
             double A = (Y1 - Y2) / (X1 - X2);
             double B = Y2 - A * X2;
-
             return (A, B);
         }
     }
-}
-
+ }
